773feda7354401c53306b12f68e3d08f75d5c10c
khs220507 <khs220507@gmail.com>
2023-12-28 08:01:10 +0900

    winform calc

--- CalcPractice/CalcPractice/Form1.cs
+++ CalcPractice/CalcPractice/Form1.cs
@@ -26,28 +26,33 @@ namespace calcPractice
 
         }
 
+        // btn0~btn9까지 클릭했을때 처리하는 메소드
         private void btnNumber_Click(object sender, EventArgs e)
         {
             Button btn = sender as Button;
             // 1. "0"일때 "01"이 나오면 안됨
-            // 2. "12"라는 숫자를 입력한 후 "+"를 누르고 다시 "3"이라고는 숫자를 누르면 결과창에는 "123"이 아닌 "3"이 나와야함
-            if (txtResult.Text=="0" || opFlag ==true)
+            // 2.  연산자가 현재 적용상태, "12"라는 숫자를 입력한 후 "+"를 누르고 다시 "3"이라고는 숫자를 누르면 결과창에는 "123"이 아닌 "3"이 나와야함
+            if (txtResult.Text=="0" || opFlag ==true) 
             {
-                txtResult.Text = btn.Text;
-                opFlag = false; // 
-                
+                txtResult.Text = btn.Text; // 덮어쓰기
+                opFlag = false; // overwirte 후 연산자 상태 초기화
             }
+
+            else if(op == ' ')
+            {
+                MessageBox.Show("연산자를 입력하세요", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
 
             // 위 두가지 경우가 아니면 "12"라는 숫자를 입력한 후 "3"을 입력하면 "123"이 나옴
             else
-                txtResult.Text = txtResult.Text + btn.Text;
-
-
+                txtResult.Text = txtResult.Text + btn.Text; // 숫자를 연속으로 입력했을때 텍스트로 이어붙이기
         }
 
         private void btnDot_Click_Click(object sender, EventArgs e)
         {
-            if (txtResult.Text.Contains("."))
+            if (txtResult.Text.Contains(".")) // "."이 있다면 함수 실행 종료, 주어진 값을 함수 호출지점으로 반환
                 return;
             else
                 txtResult.Text += ".";
@@ -85,9 +90,17 @@ namespace calcPractice
             opFlag = true;
         }
 
+        // 실제적으로 계산을 수행하는곳
         private void buttonResult_Click(object sender, EventArgs e)
         {
             Double v = Double.Parse(txtResult.Text); //
+
+            if (op == '/' && v == 0)
+            {
+                MessageBox.Show("0으로 나눌 수 없습니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             switch (op)
             {
                 case '+':
@@ -104,6 +117,7 @@ namespace calcPractice
                     break;
             }
             txtExp.Text = "";
+            op = ' ';
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
